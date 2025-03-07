1e68e7c2817532fc40a75fabe382cc7d85482ffe
Lidia Fernández <lidfercas@alum.us.es>
2025-03-07 13:23:15 +0100

    Entidad Booking implementada con relaciones comentadas

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,67 @@
+
+package acme.entities.bookings;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Booking extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				price;
+
+	@Optional
+	@ValidString(min = 4, max = 4)
+	@Automapped
+	private String				lastNibble;
+
+	//Relationships ---------------------------------------------------------
+
+	//@Mandatory
+	//@Valid
+	//@ManyToOne(optional = false)
+	//private Flight				flight;
+
+	//@Mandatory
+	//@Valid
+	//@ManyToOne(optional = false)
+	//private Customer				customer;
+
+}
