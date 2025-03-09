7950e320cb30426e675d4a8a24482187c6a1525a
mery16q <marquifer@alum.us.es>
2025-03-09 18:30:52 +0100

    Role AssistanceAgent

---
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -0,0 +1,68 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class AssistanceAgent extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				spokenLanguages;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Optional
+	@ValidString(min = 0, max = 255)
+	@Automapped
+	private String				briefBio;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				money;
+
+	@Optional
+	@ValidUrl
+	private String				photo;
+
+	//Relationships
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne(optional = false)
+	 * private Airline airline;
+	 */
+}
