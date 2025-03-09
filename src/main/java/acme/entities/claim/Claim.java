affcb4de81a177309e3fa1db20d1d1ff3d01538e
mery16q <marquifer@alum.us.es>
2025-03-09 19:20:23 +0100

    Añadir la entidad Claim

---
+++ src/main/java/acme/entities/claim/Claim.java
@@ -0,0 +1,61 @@
+
+package acme.entities.claim;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Claim extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				passengerEmail;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private ClaimType			type;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				indicator;
+
+	//Relations
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne(optional = false)
+	 * private AssistanceAgent assistanceAgent;
+	 */
+}
