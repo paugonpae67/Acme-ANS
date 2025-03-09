af33e4f33a81a1281799a928376d1698cbc48fd8
iratxe-pm <iratxepm04@gmail.com>
2025-03-09 11:43:00 +0100

    Refactor: corrección de las relaciones de flightAssignment

--- src/main/java/acme/entities/flightAssignment/FlightAssignment.java
+++ src/main/java/acme/entities/flightAssignment/FlightAssignment.java
@@ -15,7 +15,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
-import acme.entities.aircrafts.Aircraft;
+import acme.entities.flightCrewMembers.FlightCrewMembers;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -51,7 +51,7 @@ public class FlightAssignment extends AbstractEntity {
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private Aircraft				aircraft;
+	private FlightCrewMembers		flightCrewMembers;
 
 	/*
 	 * @Mandatory
