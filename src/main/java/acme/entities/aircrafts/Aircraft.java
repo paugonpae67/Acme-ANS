3094bfefb68098ef1df98056a2507c2da055a396
iratxe-pm <iratxepm04@gmail.com>
2025-03-10 19:27:48 +0100

    feat: tablas de datos airline y airport

--- src/main/java/acme/entities/aircrafts/Aircraft.java
+++ src/main/java/acme/entities/aircrafts/Aircraft.java
@@ -3,6 +3,7 @@ package acme.entities.aircrafts;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -11,6 +12,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -53,9 +55,9 @@ public class Aircraft extends AbstractEntity {
 
 	//Relationships ---------------------------------------------------------
 
-	//@Mandatory
-	//@Valid
-	//@ManyToOne(optional = false)
-	//private Airline				airline;
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 
 }
