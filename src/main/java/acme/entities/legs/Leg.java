3ebb08ec87a1f95f8463385306e1105d5443ea10
Fernando-Partal <fpgsfc@gmail.com>
2025-03-11 13:00:55 +0100

    Entidad Leg terminada

--- src/main/java/acme/entities/legs/Leg.java
+++ src/main/java/acme/entities/legs/Leg.java
@@ -15,6 +15,7 @@ import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
+import acme.constraints.ValidFlightNumber;
 import acme.entities.aircrafts.Aircraft;
 import acme.entities.airports.Airport;
 import acme.entities.flights.Flight;
@@ -32,6 +33,7 @@ public class Leg extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
+	@ValidFlightNumber
 	@Column(unique = true)
 	private String				flightNumber;
 
