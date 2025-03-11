d2d1c415f26d40ae26021db4f224a06142563846
paula <paulagonzalezllanes@gmail.com>
2025-03-11 17:14:51 +0100

    Dashboard technician

---
+++ src/main/java/acme/forms/TechnicianDashboard.java
@@ -0,0 +1,33 @@
+
+package acme.forms;
+
+import java.util.List;
+
+import acme.client.components.basis.AbstractForm;
+import acme.client.components.datatypes.Money;
+import acme.entities.aircrafts.Aircraft;
+import acme.entities.maintenanceRecords.MaintenanceRecord;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TechnicianDashboard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	Integer						numberMaintenanceRecordPending;
+	Integer						numberMaintenanceRecordInProgress;
+	Integer						numberMaintenanceRecordCompleted;
+	MaintenanceRecord			recordWithNearestInspection;
+	List<Aircraft>				top5AircraftsWithMostTasks;
+	Money						averageEstimatedCostLastYear;
+	Money						minimumEstimatedCostLastYear;
+	Money						maximumEstimatedCostLastYear;
+	Money						sTDDEVEstimatedCostLastYear;
+	Double						averageEstimatedDurationTask;
+	Integer						minimumEstimatedDurationTask;
+	Integer						maximumEstimatedDurationTask;
+	Double						sTDDEVEstimatedDurationTask;
+
+}
