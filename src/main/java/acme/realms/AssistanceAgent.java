1dd3974756e740c26d9a7ee13638f0fa3c5ea02d
mery16q <marquifer@alum.us.es>
2025-03-11 11:23:34 +0100

    Correción customValidator

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,10 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -20,7 +18,6 @@ import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
 import acme.constraints.ValidAssistanceAgent;
-import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -62,9 +59,12 @@ public class AssistanceAgent extends AbstractRole {
 	private String				photo;
 
 	//Relationships
-
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Airline				airline;
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne(optional = false)
+	 * private Airline airline;
+	 */
 }
