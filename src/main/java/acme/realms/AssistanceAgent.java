4b8d733f55732bfc6073555c6eea4057f9752f54
mery16q <marquifer@alum.us.es>
2025-03-11 11:18:21 +0100

    Relacion de assistance agent con airoline

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -17,11 +19,14 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidAssistanceAgent;
+import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ValidAssistanceAgent
 @Entity
 public class AssistanceAgent extends AbstractRole {
 
@@ -57,12 +62,9 @@ public class AssistanceAgent extends AbstractRole {
 	private String				photo;
 
 	//Relationships
-	/*
-	 * @Mandatory
-	 * 
-	 * @Valid
-	 * 
-	 * @ManyToOne(optional = false)
-	 * private Airline airline;
-	 */
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }
