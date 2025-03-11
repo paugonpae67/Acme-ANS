80260cb2aa9c936e0757e65fe43e7148790d9cc9
Lidia Fernández <lidfercas@alum.us.es>
2025-03-11 18:45:57 +0100

    Sample Data de Customer

--- src/main/java/acme/constraints/CustomerValidator.java
+++ src/main/java/acme/constraints/CustomerValidator.java
@@ -21,12 +21,12 @@ public class CustomerValidator extends AbstractValidator<ValidCustomer, Customer
 		boolean result;
 
 		if (customer == null)
-			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+			super.state(context, true, "*", "javax.validation.constraints.NotNull.message");
 		else {
 			String name = customer.getIdentity().getName();
 			String surname = customer.getIdentity().getSurname();
 			if (customer.getIdentifier() == null || !customer.getIdentifier().matches("^[A-Z]{2,3}\\d{6}$"))
-				super.state(context, false, "*", "acme.validation.identifier.format");
+				super.state(context, true, "*", "acme.validation.identifier.format");
 			else {
 				String identifier = customer.getIdentifier();
 				char identifierFirstChar = Character.toUpperCase(identifier.charAt(0));
@@ -34,8 +34,8 @@ public class CustomerValidator extends AbstractValidator<ValidCustomer, Customer
 				char nameFirstChar = Character.toUpperCase(name.charAt(0));
 				char surnameFirstChar = Character.toUpperCase(surname.charAt(0));
 
-				super.state(context, !(identifierFirstChar == nameFirstChar), "*", "acme.validation.identifier.initial-mismatch");
-				super.state(context, !(identifierSecondChar == surnameFirstChar), "*", "acme.validation.identifier.initial-mismatch");
+				super.state(context, identifierFirstChar == nameFirstChar, "*", "acme.validation.identifier.initial-mismatch");
+				super.state(context, identifierSecondChar == surnameFirstChar, "*", "acme.validation.identifier.initial-mismatch");
 			}
 		}
 		result = !super.hasErrors(context);
