4bf2bfbdbce5a0b407276e7ac27d907682104d14
mery16q <marquifer@alum.us.es>
2025-03-11 11:15:51 +0100

    CustomValid para el employeeCode

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -28,7 +28,7 @@ public class AssistanceAgent extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
 	private String				employeeCode;
 
