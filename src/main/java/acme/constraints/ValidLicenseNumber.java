7c6f9175a4a4193a65356d84a8df5c7a54283b16
paula <paulagonzalezllanes@gmail.com>
2025-03-12 12:10:55 +0100

    Test finalizados Paula y validadores

--- src/main/java/acme/constraints/ValidLicenseNumber.java
+++ src/main/java/acme/constraints/ValidLicenseNumber.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -18,15 +19,13 @@ import org.hibernate.validator.constraints.Length;
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 
-@Length(min = 6, max = 16)
-@Pattern(regexp = "^\\+?\\d{6,15}$")
-public @interface ValidPhone {
-
-	// Standard validation properties -----------------------------------------
+@NotBlank
+@Length(min = 8, max = 9)
+@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$")
+public @interface ValidLicenseNumber {
 
 	String message() default "";
 
 	Class<?>[] groups() default {};
 	Class<? extends Payload>[] payload() default {};
-
 }
