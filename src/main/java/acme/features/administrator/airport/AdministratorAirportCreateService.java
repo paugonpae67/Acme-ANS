25c8628ca75f8a1c348402a453af815692c6e2cd
Fernando-Partal <fpgsfc@gmail.com>
2025-04-01 18:41:35 +0200

    Redistribución de Airport

--- src/main/java/acme/features/administrator/airport/AdministratorAirportCreateService.java
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportCreateService.java
@@ -1,5 +1,5 @@
 
-package acme.features.airport;
+package acme.features.administrator.airport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,7 +12,7 @@ import acme.entities.airports.Airport;
 import acme.entities.airports.OperationalType;
 
 @GuiService
-public class AdministratorAirportUpdateService extends AbstractGuiService<Administrator, Airport> {
+public class AdministratorAirportCreateService extends AbstractGuiService<Administrator, Airport> {
 
 	@Autowired
 	private AdministratorAirportRepository airportRepository;
@@ -25,13 +25,10 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 
 	@Override
 	public void load() {
-		Airport airport;
-		int id;
-
-		id = super.getRequest().getData("id", int.class);
-		airport = this.airportRepository.findAirportById(id);
+		Airport airport = new Airport();
 
 		super.getBuffer().addData(airport);
+
 	}
 
 	@Override
@@ -42,16 +39,15 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 	@Override
 	public void validate(final Airport airport) {
 		Airport existAirport = this.airportRepository.findAirportByIataCode(airport.getIataCode());
-		boolean valid = existAirport == null || existAirport.getId() == airport.getId();
 		boolean confirmation;
 		confirmation = super.getRequest().getData("confirmation", boolean.class);
 		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
-		super.state(valid, "iataCode", "acme.validation.airport.form.error.duplicateIata");
-
+		super.state(existAirport == null, "iataCode", "acme.validation.airport.form.error.duplicateIata");
 	}
 
 	@Override
 	public void perform(final Airport airport) {
+
 		this.airportRepository.save(airport);
 	}
 
@@ -65,4 +61,5 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 		dataset.put("scopes", choices);
 		super.getResponse().addData(dataset);
 	}
+
 }
