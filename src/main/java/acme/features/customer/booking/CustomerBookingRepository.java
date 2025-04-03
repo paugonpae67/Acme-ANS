cfab12504b31364fec989e026d1010a72354ad21
Lidia Ning <lidfercas@alum.us.es>
2025-04-03 22:10:01 +0200

    Operaciones con aircraft terminadas. Validaciones y arreglos generales

--- src/main/java/acme/features/customer/booking/CustomerBookingRepository.java
+++ src/main/java/acme/features/customer/booking/CustomerBookingRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.bookings.Booking;
+import acme.entities.bookings.BookingRecord;
 import acme.entities.flights.Flight;
 import acme.entities.passengers.Passenger;
 
@@ -28,5 +29,8 @@ public interface CustomerBookingRepository extends AbstractRepository {
 
 	@Query("SELECT br.passenger FROM BookingRecord br WHERE br.booking.id = :bookingId")
 	Collection<Passenger> findPassengersByBookingId(int bookingId);
+
+	@Query("SELECT br FROM BookingRecord br WHERE br.booking.id = :bookingId")
+	Collection<BookingRecord> findBookingBookingRecordsByBookingId(int bookingId);
 
 }
