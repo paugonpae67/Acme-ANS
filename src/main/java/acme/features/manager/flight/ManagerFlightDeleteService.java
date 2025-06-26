a9704240a0eb699dcce82562bef4fe9ef5a6f37a
Fernando-Partal <fpgsfc@gmail.com>
2025-06-27 00:12:09 +0200

    Error Solucionado

--- src/main/java/acme/features/manager/flight/ManagerFlightDeleteService.java
+++ src/main/java/acme/features/manager/flight/ManagerFlightDeleteService.java
@@ -6,9 +6,12 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.flights.Flight;
+import acme.entities.flights.FlightIndication;
 import acme.entities.legs.Leg;
 import acme.features.manager.leg.ManagerLegRepository;
 import acme.realms.Manager;
@@ -58,6 +61,11 @@ public class ManagerFlightDeleteService extends AbstractGuiService<Manager, Flig
 
 	@Override
 	public void validate(final Flight flight) {
+		Collection<Leg> legs = this.managerLegRepository.findLegsByflightId(flight.getId());
+
+		boolean allLegsInDraft = legs.stream().allMatch(Leg::isDraftMode);
+
+		super.state(allLegsInDraft, "*", "manager.flight.form.error.published-leg");
 	}
 
 	@Transactional
@@ -92,5 +100,19 @@ public class ManagerFlightDeleteService extends AbstractGuiService<Manager, Flig
 
 	@Override
 	public void unbind(final Flight flight) {
+		Dataset dataset = super.unbindObject(flight, "tag", "indication", "cost", "description");
+
+		dataset.put("scheduledDeparture", flight.getScheduledDeparture());
+		dataset.put("scheduledArrival", flight.getScheduledArrival());
+		dataset.put("originCity", flight.getOriginCity());
+		dataset.put("destinationCity", flight.getDestinationCity());
+		int layovers = flight.getNumberOfLayovers();
+		dataset.put("numberOfLayovers", layovers >= 0 ? layovers : 0);
+
+		dataset.put("draftMode", flight.isDraftMode());
+		dataset.put("indications", SelectChoices.from(FlightIndication.class, flight.getIndication()));
+
+		super.getResponse().addData(dataset);
 	}
+
 }
